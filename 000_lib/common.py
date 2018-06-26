d1062be11de2e83068f9ee97e7cb69754cc221cb
tanukichi123 <onemiudo@gmail.com>
2018-06-27 06:00:46 +0900

    _をスペースに変換を追加

--- 000_lib/common.py
+++ 000_lib/common.py
@@ -25,11 +25,6 @@ def jikou(exe_path=""):
         args=[[exe_path]]
         p=subprocess.Popen(args)
 
-if __name__ == '__main__':
-    jikou("sikuli")
-
-
-
 def delete_equal():
     b=""
     tex=pyperclip.paste()
@@ -44,6 +39,11 @@ def delete_kaigyo():
     tex=tex.replace("\r\n","")
     pyperclip.copy(tex)
 
+def delete_under_bar():
+    tex=pyperclip.paste()
+    tex=tex.replace("_"," ")
+    pyperclip.copy(tex)
+
 if __name__ == '__main__':
     """test_delete_equal
     """
@@ -54,5 +54,14 @@ if __name__ == '__main__':
     """delete_kaigyo
     """
     test='self.chg_posi = self.obs_posi[-self.step_len:]\r\nself.chg_posi_var = self.obs_posi_var[-self.step_len:]\r\nself.chg_posi_entry_cover = self.obs_posi_entry_cover[-self.step_len:]\r\nself.chg_price = self.obs_price[-self.step_len:]\r\nself.chg_price_mean = self.obs_price_mean[-self.step_len:]\r\nself.chg_reward_fluctuant = self.obs_reward_fluctuant[-self.step_len:]\r\nself.chg_makereal = self.obs_makereal[-self.step_len:]\r\nself.chg_reward = self.obs_reward[-self.step_len:]'
+    test='self.chg_posi self.chg_posi_var self.chg_posi_entry_cover self.chg_price self.chg_price_mean self.chg_reward_fluctuant self.chg_makereal self.chg_reward '
+    pyperclip.copy(test)
     delete_kaigyo()
     print(pyperclip.paste())
+    """delete_under_bar
+    """
+    test="delete_under_bar"
+    pyperclip.copy(test)
+    delete_under_bar()
+    print(pyperclip.paste())
+
